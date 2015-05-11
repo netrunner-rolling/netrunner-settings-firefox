777407ab7e812102ff4e9c80ff089a42e937b3ae
Leszek Lesner <leszek.lsner@web.de>
2015-05-11 12:26:58 +0200

    New Video Download Helper settings Profile update

---
+++ etc/skel/.mozilla/firefox/urmsktfc.default/extensions/staged/anttoolbar@ant.com/chrome/content/grabber/queryobserver.js
@@ -0,0 +1,110 @@
+/**
+ * queryobserver.js, 2008
+ * @author Zak
+ * @contributor Igor Chornous ichornous@heliostech.hk
+ */
+
+/**
+ * @namespace antvd
+ */
+var antvd = (function(antvd) {
+    if (!antvd.AntLib)
+        antvd.AntLib = AntLib;
+
+    /**
+     * @class AntQueryObserver
+     */
+    function AntQueryObserver()
+    {
+        const Ci = Components.interfaces;
+        const Cc = Components.classes;
+
+        // {{{ Search strategies
+        /**
+         * DefaultSearchStrategy must remain the last one in the list
+         *
+         * @name searchStrategy
+         * @type Array<ISearchStrategy>
+         */
+        var searchStrategies = [
+            new antvd.YtSearchStrategy()
+            , new antvd.DmSearchStrategy()
+            , new antvd.DefaultSearchStrategy()
+        ];
+        // }}}
+
+        // {{{ observe method
+        /**
+         * The function called by firefox, entry point of this class
+         * @param request       The object containing the request params
+         * @param topic         The specific event that trigger the function
+         * @param data          the request data (seems to be empty)
+         */
+        this.observe = function(aRequest, topic, data)
+        {
+            if (topic.substring(0, 16) != "http-on-examine-")
+                return;
+
+            if (!gBrowser.getBrowserForDocument)
+                return;
+
+            var httpChannel = aRequest.QueryInterface(Ci.nsIHttpChannel);
+            if ((httpChannel.responseStatus < 200)
+                || (httpChannel.responseStatus > 299))
+                return;
+
+            var document = antvd.AntLib.getDocumentByRequest(httpChannel);
+            if (!document)
+                return;
+
+            // Ensure that the request has an associated browser
+            // In some cases firefox may prefetch the page
+            if (!gBrowser.getBrowserForDocument(document))
+                return;
+
+            var strategy = getSearchStrategy(document, httpChannel);
+            if (!strategy)
+                return;
+
+            try
+            {
+                strategy.search(
+                    document
+                    , httpChannel
+                    , function(request, erase) {
+                        AntGrabber.foundFlvLink(document, request, erase);
+                    });
+            }
+            catch (e)
+            {
+                antvd.AntLib.logError("Unexpected program failure", e);
+            }
+        };
+        // }}}
+
+        // {{{ getSearchStrategy method
+        /**
+         * Looks up for an appropriate detecting strategy
+         *
+         * @private
+         * @member getSearchStrategy
+         * @param {Document} document Owning document
+         * @param {nsIHttpChannel} channel
+         * @returns {ISearchStrategy}
+         */
+        var getSearchStrategy = function(document, channel)
+        {
+            for each (var strategy in searchStrategies)
+            {
+                if (strategy.isApplicable(document, channel))
+                    return strategy;
+            }
+
+            return null;
+        };
+        // }}}
+    };
+
+    /** @expose */ antvd.AntQueryObserver = AntQueryObserver;
+    return antvd;
+})(antvd);
