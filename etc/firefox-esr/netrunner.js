9141d71ae5df8b7241098cb2ee8a7e82bb0b42a3
Rohan Garg <rohan@garg.io>
2017-06-23 14:50:25 +0200

    Update with missing settings

--- etc/firefox-esr/netrunner.js
+++ etc/firefox-esr/netrunner.js
@@ -1,12 +1,26 @@
+pref("accessibility.typeaheadfind.flashBar", 0);
+pref("app.update.disable_button.showUpdateHistory", false);
+
+pref("browser.cache.disk.smart_size.use_old_max", false);
+pref("browser.cache.frecency_experiment", 3);
+
+pref("browser.customizemode.tip0.shown", true);
+
 pref("browser.disableResetPrompt", true);
 
 pref("browser.download.dir", "///~/");
 pref("browser.download.lastDir", "~");
+pref("browser.download.panel.firstSessionCompleted", true);
+pref("browser.download.useDownloadDir", false);
 
 pref("browser.places.smartBookmarksVersion", -1);
 
+pref("browser.preferences.advanced.selectedTabIndex", 0);
+
 pref("browser.showMenubar", true);
 pref("browser.showPersonalToolbar", true);
+
+pref("browser.sessionstore.interval", 60000);
 
 pref("browser.startup.firstrunSkipsHomepage", true);
 pref("browser.startup.homepage","data:text/plain,browser.startup.homepage=http://google.com");
@@ -23,9 +37,19 @@ pref("browser.urlbar.doubleClickSelectsAll", false);
 pref("browser.urlbar.formatting.enabled", false);
 pref("browser.urlbar.trimURLs", false);
 
+pref("font.name.monospace.x-western", "Liberation Mono");
+pref("font.name.serif.x-western", "Liberation Serif");
+pref("font.size.fixed.x-western", 13);
+
 pref("mousewheel.min_line_scroll_amount", 25);
 pref("mousewheel.system_scroll_override_on_root_content.enabled", true);
+
+pref("pref.privacy.disable_button.view_passwords", false);
 
 pref("security.insecure_field_warning.contextual.enabled", false);
 
 pref("startup.homepage_welcome_url", "https://www.google.com");
+
+pref("toolkit.scrollbox.smoothScroll", false);
+
+pref("ui.key.menuAccessKeyFocuses", false);
