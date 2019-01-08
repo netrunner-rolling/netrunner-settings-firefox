699c605a643a09f8683c6cd89d6cd31552e928cb
nulogicsystems <james@nulogicsystems.com>
2019-01-08 07:42:12 -0500

    Update storage.js
    as per: https://github.com/blue-systems/netrunner-releases/issues/117

--- etc/skel/.mozilla/firefox/firefox.default/browser-extension-data/uBlock0@raymondhill.net/storage.js
+++ etc/skel/.mozilla/firefox/firefox.default/browser-extension-data/uBlock0@raymondhill.net/storage.js
@@ -1,1 +1,1 @@
-{"user-filters":"www.google.*###cnsm` > g-loading-icon\ngoogle.*###cnsm > g-loading-icon\n","assets/user/filters.txt":"www.google.*###cnsm` > g-loading-icon\ngoogle.*###cnsm > g-loading-icon\n","hostnameSwitchesString":"no-large-media: behind-the-scene false\nno-scripting: behind-the-scene false"}
+{"user-filters":"www.google.*###cnsm` > g-loading-icon\ngoogle.*###cnsm > g-loading-icon\n","assets/user/filters.txt":"www.google.*###cnsm` > g-loading-icon\ngoogle.*###cnsm > g-loading-icon\n"}
